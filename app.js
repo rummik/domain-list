c5a4a9c85f58af188ff99877a7e2c066f7cdf479
rummik <r@9k1.us>
2014-05-20 00:55:52 -0400

    Some initial code

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var cloudflare = require('cloudflare');
+var config = require('./config');
+
+var fs = require('fs');
+var app = express();
+var cf = cloudflare.createClient(config.cloudflare);
+
+var list = '';
+var index = '';
+
+app.get('/', function(req, res) {
+	if (!index || process.env.NODE_ENV == 'development')
+		index = fs.readFileSync('public/index.html').toString('utf8');
+
+	res.send(index.replace('{{list}}', list.join('\n')).replace('{{files}}', 0).replace('{{dirs}}', list.length + 2));
+});
+
+app.use(express.static('public'));
+
+(function get(fb) {
+	cf.listDomainRecords('9k1.us', function (err, records) {
+		if (err)
+			return console.error(err);
+
+		list = records
+			.filter(function(record) {
+				return ['A', 'AAAA', 'CNAME'].indexOf(record.type) >= 0 && /\.app$/.test(record.display_name);
+			})
+
+			.sort(function(a, b) {
+				if (a.name > b.name)
+					return 1;
+
+				if (a.name < b.name)
+					return -1;
+
+				return 0;
+			})
+
+			.filter(function(value, i, array) {
+				return i == 0 || array[i - 1].name != value.name;
+			})
+
+			.map(function(record) {
+				return [
+					'31.01.2101 23:59',
+					'    &lt;DIR&gt;          ',
+					record.name,
+				].join('');
+			});
+
+	});
+
+	setTimeout(get, 60 * 60 * 1000);
+})();
+
+app.listen(process.env.PORT || 9001);
---
+++ readme.md
@@ -0,0 +1,41 @@
+Domain List [![Builds][]][travis] [![Deps][]][gemnasium] [![Donations][]][gittip]
+===========
+Retrieve a list of subdomains from CloudFlare
+
+[Builds]: http://img.shields.io/travis-ci/rummik/domain-list.png "Build Status"
+[travis]: https://travis-ci.org/rummik/domain-list
+[Deps]: https://gemnasium.com/rummik/domain-list.png "Dependency Status"
+[gemnasium]: https://gemnasium.com/rummik/domain-list
+[Donations]: http://img.shields.io/gittip/rummik.png
+[gittip]: https://www.gittip.com/rummik/
+
+
+## Getting Started
+_(Coming soon)_
+
+
+## Documentation
+_(Coming soon)_
+
+
+## Examples
+_(Coming soon)_
+
+
+## Contributing
+Please see the [Chameleoid Styleguide][] before contributing.
+
+Take care to maintain the existing coding style.  Add unit tests for any new or
+changed functionality.  Lint and test your code using [Grunt][].
+
+[Chameleoid Styleguide]: https://github.com/chameleoid/style
+[Grunt]: http://gruntjs.com/
+
+
+## Release History
+_(Nothing yet)_
+
+
+## License
+Copyright (c) 2014
+Licensed under the MPL license.
